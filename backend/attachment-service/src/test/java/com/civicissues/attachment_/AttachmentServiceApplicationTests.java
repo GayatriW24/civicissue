acc6768ee897b9ccfee45fd6e984619e703fe47d
GayatriW24 <gayatriwakchaure2419@gmail.com>
2025-11-17 01:03:06 +0530

    add fresh attachemnt-service with correct package

---
+++ backend/attachment-service/src/test/java/com/civicissues/attachment_/AttachmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civicissues.attachment_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttachmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
