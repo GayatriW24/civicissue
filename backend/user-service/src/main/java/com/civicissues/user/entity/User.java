7395a1e8af4919dac0ff4e32af4b81e2c21b5f82
GayatriW24 <gayatriwakchaure2419@gmail.com>
2025-11-25 19:24:20 +0530

    added dto and user

--- backend/user-service/src/main/java/com/civicissues/user/entity/User.java
+++ backend/user-service/src/main/java/com/civicissues/user/entity/User.java
@@ -1,17 +1,30 @@
 package com.civicissues.user.entity;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="users")
-public class User extends BaseEntity{
+@Getter
+@Setter
+public class User extends BaseEntity implements UserDetails{
+	
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
 	private Long id;
 	@Column(length=50,nullable=false)
 	private String name;
@@ -25,6 +38,26 @@ public class User extends BaseEntity{
 	private String address;
 	@Enumerated(EnumType.STRING)
 	private Role role;
+	
+	public User(String name, String email, String password, String phone, String address) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.address = address;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return List.of(new SimpleGrantedAuthority(this.role.name()));
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
 	
 
 }
